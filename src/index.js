0ce28b529cf04f5a844bf025f6ff465f7f89a829
yutasaito <test@gmail.com>
2019-10-24 12:50:27 +0900

    install eslint-loader

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './style.css'
-import './style.scss'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.css';
+import './style.scss';
 
-console.log('I am printing logs.')
-
-alert('Hi, source-map!')
-
+console.log('I am printing logs.');
 ReactDOM.render(
-	<div>Hello, Reaact!</div>,
-	document.getElementById('root')
-)
+  <div>Hello, Reaact!</div>,
+  document.getElementById('root'),
+);
